a99bdf66227c42b1ed5f4f548602006728d6b0c4
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-09-03 20:12:24 -0300

    e2e - Acrescentando outros valores

--- scr/e2e/csv.e2e.spec.js
+++ scr/e2e/csv.e2e.spec.js
@@ -16,7 +16,18 @@ test('Testando api transactions', async ({ request }) => {
                 invoice: expect.any(Number),
                 transaction: expect.objectContaining({
                     cd_produto: expect.any(String),
-                    in_estorno: expect.any(String)
+                    in_estorno: expect.any(String),
+                    cd_produto: expect.any(String),
+                    tp_valor: expect.any(String),
+                    cd_empresa: expect.any(String),
+                    round: expect.any(String),
+                    nr_dctoorigem: expect.any(String),
+                    nr_sequencia: expect.any(String),
+                    cd_valor: expect.any(String),
+                    cd_historico: expect.any(String),
+                    in_estorno: expect.any(String),
+                    dt_movimento: expect.any(String),
+                    dt_cadastro: expect.any(String), 
                 }),
             })
         ])
